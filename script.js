ac85429f4d88d97fe45c154f46dfeddda6f9be28
Tina Shi <init.defaultBranch>
2023-02-27 15:37:55 +1100

    add javascript event, edited HTML to follow

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+function bigImg(x) {
+  x.style.height = "300px";
+  x.style.width = "300px";
+}
+        
+function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+}
